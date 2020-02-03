5d7b94342838244348cf1a9ca235bbb8374b128c
adilpratama <adilpratama71@yahoo.com>
2020-02-03 17:41:00 +0700

    create todo models

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,13 +1,66 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Todo = sequelize.define('Todo', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    status: DataTypes.BOOLEAN,
-    due_date: DataTypes.DATE
-  }, {});
+  const Model = sequelize.Sequelize.Model;
+  class Todo extends Model {}
+  Todo.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [1],
+            msg: "Title at least have 1 characters"
+          },
+          notNull: {
+            args: true,
+            msg: "Title should not be empty"
+          }
+        }
+      },
+      UserId: {
+        type: DataTypes.INTEGER
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [1],
+            msg: "description at least have 1 characters"
+          },
+          notNull: {
+            args: true,
+            msg: "description should not be empty"
+          }
+        }
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      due_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          checkDate: function(v) {
+            const date = new Date(v);
+            if (date < new Date()) {
+              throw new Error("due_date cannot be less than the current date");
+            }
+          },
+          notNull: {
+            args: true,
+            msg: "due_date should not be empty"
+          }
+        }
+      }
+    },
+    { sequelize }
+  );
   Todo.associate = function(models) {
     // associations can be defined here
+    Todo.belongsTo(models.User);
   };
   return Todo;
 };
