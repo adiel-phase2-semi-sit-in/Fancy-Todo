9556df2323c15401e6480b81ddd3d509a2473531
adilpratama <adilpratama71@yahoo.com>
2020-02-06 13:53:40 +0700

    heroku again

--- client/index.js
+++ client/index.js
@@ -1,23 +1,12 @@
-function onSignIn(googleUser) {
-  const id_token = googleUser.getAuthResponse().id_token;
-  axios({
-    method: "post",
-    url: "http://localhost:3000/users/googleSignIn",
-    headers: {
-      token: id_token
-    }
-  })
-    .then(({ data }) => {
-      localStorage.setItem("token", data.token);
-    })
-    .catch(err => console.log(err));
-}
-
 $(document).ready(function() {
   $("#logout").click(function() {
-    const auth = gapi.auth2.getAuthInstance();
-    auth.signOut().then(response => {
-      localStorage.clear();
-    });
+    onSignOut();
   });
+  if (localStorage.token) {
+    $("#dashboard").show();
+    $("#landing-page").hide();
+  } else {
+    $("#dashboard").hide();
+    $("#landing-page").show();
+  }
 });
